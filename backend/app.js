5def45de0af22b834dd1148aaa5c4cfa6678117a
Mohsin <mohsinshah3171@gmail.com>
2024-01-02 00:35:31 +0500

    Backend: Initialize

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const app = express();
+
+// Handle Uncaught Exceptions
+process.on("uncaughtException", e => {
+  console.log(e);
+})
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ 
+  limit: "10mb",
+  extended: true,
+  strict: true
+}));
+
+app.use(cors());
+app.use(helmet());
+
+// Catch 404
+app.use((req, res) => {
+  return res.status(404).send("Error 404, Route not found");
+});
+
+// Handle Errors
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  req.log.error(err);
+  return res.status(500).send(err.message);
+});
+
+module.exports = app;
---
+++ backend/config/index.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+// import .env variables
+require("dotenv").config({
+  path: path.join(__dirname, "../.env"),
+  example: path.join(__dirname, "../.env.example"),
+});
+
+module.exports = {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  sql: {
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE
+  }
+};
